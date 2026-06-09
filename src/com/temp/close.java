efe1c4ac8c1ab0b5ea8a4fca6d6dbf3915f89072
sandeep <24415.cs@rmkcet.ac.in>
2026-06-09 19:55:15 +0530

    9.6.2026

---
+++ src/com/temp/close.java
@@ -0,0 +1,8 @@
+package com.temp;
+
+public class close {
+public static void main(String...args) {
+	float a=527f;
+	System.out.println(a);
+}
+}
---
+++ src/com/temp/openone.java
@@ -0,0 +1,8 @@
+package com.temp;
+
+public class openone {
+public static void main(String...args) {
+	double b=6548.3978;
+	System.out.println(b);
+}
+}
