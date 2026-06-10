052cf34b908afd4b33501170e545716cbb5812ec
sandeep <24415.cs@rmkcet.ac.in>
2026-06-10 19:27:23 +0530

    today i learned type casting and incrementation decrementation operations

---
+++ src/com/temp/operations.java
@@ -0,0 +1,12 @@
+package com.temp;
+
+public class operations {
+public static void main(String...args) {
+	int a=10,b=15;
+	int c =a++ + --a + b++ + b--;
+	System.out.println(c);
+	System.out.println(a);
+	System.out.println(b);
+	
+}
+}
---
+++ src/com/temp/typecasting.java
@@ -0,0 +1,11 @@
+package com.temp;
+
+public class typecasting {
+public static void main(String...args) {
+	int a=45;
+	int b;
+	b=a;
+	System.out.println(a);
+	System.out.println(b);
+}
+}
