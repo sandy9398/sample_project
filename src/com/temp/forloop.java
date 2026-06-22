521ae0665782c370efa1627147c6835334f1e35d
sandeep <24415.cs@rmkcet.ac.in>
2026-06-22 19:37:38 +0530

    pattrenprograming

--- src/com/temp/forloop.java
+++ src/com/temp/forloop.java
@@ -3,6 +3,7 @@ package com.temp;
 public class forloop {
 public static void main(String...args) {
 	for(int i=0;i<=10;i++) {
+		
 		System.out.println(i);
 	}
 }
---
+++ src/com/temp/pattren.java
@@ -0,0 +1,13 @@
+package com.temp;
+
+public class pattren {
+public static void main(String...args) {
+	for(int i=0;i<5;i++) {
+		for(int j=0;j<5;j++) {
+			System.out.print(i+j+1 + " ");
+			}
+		System.out.println();
+		}
+	}
+}
+
