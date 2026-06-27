2db1c26bac2e46b3823bad34b8741dbf6c7d9398
sandeep <24415.cs@rmkcet.ac.in>
2026-06-27 22:35:59 +0530

    pattern program with examples in detail

---
+++ src/com/temp/oneeeeeeeee.java
@@ -0,0 +1,25 @@
+package com.temp;
+
+public class oneeeeeeeee {
+	public static void main(String...args) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		if(i %2 == 0) {
+			System.out.print(i+1);
+		}
+		for(int j=1;j<=n;j++) {
+			
+			System.out.print(i);
+			
+			
+			}if(i %2 != 0) {
+				System.out.print(i+1);
+			}
+			System.out.println();
+			
+	} 
+	
+		
+	}}
+	
+
